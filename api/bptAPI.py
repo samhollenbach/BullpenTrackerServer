7752890f2d83e21370fe33ede57c236abfddcbfe
Sam Hollenbach <shollenb@macalester.edu>
2018-11-28 15:59:15 -0600

    reconfigure

--- api/bptAPI.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Resource, Api
-
-class BullpenTrackerAPI(object):
-
-	def make_routes(self, application):
-		@application.route('/pitchers', methods=['GET'])
-		def test():
-			return 'welkgnae'
