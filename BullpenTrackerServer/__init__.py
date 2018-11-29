d8c3baf6429ffb3e0fde6382a32691c222aa7880
Sam Hollenbach <shollenb@macalester.edu>
2018-11-28 20:59:39 -0600

    new routes method

--- BullpenTrackerServer/__init__.py
+++ BullpenTrackerServer/__init__.py
@@ -3,10 +3,7 @@ from flask import Flask
 app = Flask(__name__, template_folder='website/templates', static_folder='website/static')
 app.debug = True
 
-import BullpenTrackerServer.api.bptAPI
-import BullpenTrackerServer.website.bptweb
+import BullpenTrackerServer.api.bptAPI as api
+import BullpenTrackerServer.website.bptweb as web
 
-
-
-if __name__ == '__main__':
-	app.run()
+web.make_routes(app)
--- BullpenTrackerServer/website/bptweb.py
+++ BullpenTrackerServer/website/bptweb.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template
 
-from BullpenTrackerServer import app
+#from BullpenTrackerServer import app
 
 
+def make_routes(app):
 
-@app.route('/', methods=['GET'])
-def home():
-	return render_template('index.html')
-
-
-@app.route('/about', methods=['GET'])
-def about():
-	return render_template('about.html')
+	@app.route('/', methods=['GET'])
+	def home():
+		return render_template('index.html')
 
 
-@app.route('/login', methods=['GET'])
-def login():
-	return render_template('login.html')
+	@app.route('/about', methods=['GET'])
+	def about():
+		return render_template('about.html')
+
+
+	@app.route('/login', methods=['GET'])
+	def login():
+		return render_template('login.html')
