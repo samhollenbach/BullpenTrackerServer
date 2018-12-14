04e9e4a4e28bd9db9a90e4e4682885f57de736de
Sean <sean.cheng@intelluslearning.com>
2018-12-14 14:59:37 -0600

    debugged chart refresh

--- BullpenTrackerServer/website/static/js/chart.js
+++ BullpenTrackerServer/website/static/js/chart.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
     });
 
     function ajax(b_tokens) {
-
+        $(".chart_container").html("");
         var all_pitches = [];
 
         for (i = 0; i < b_tokens.length; i++) {
@@ -108,6 +108,8 @@ $(document).ready(function() {
     };
 
     function processData(pitch_data) {
+        types = {};
+        pitching_data = {};
         var pitch_count = 1;
 
         for (i = 0; i < pitch_data.length; i++) {
@@ -120,6 +122,8 @@ $(document).ready(function() {
             else if (pitch_type == "X") {pitch_type = "Change-Up"}
             else if (pitch_type == "S") {pitch_type = "Slider"}
             else if (pitch_type == "B") {pitch_type = "Curveball"}
+            else if (pitch_type == "C") {pitch_type = "Cutter"}
+            else if (pitch_type == "2") {pitch_type = "2-Seam"}
 
             if (pitching_data[pitch_type] === undefined) {
                 pitching_data[pitch_type] = [];
@@ -156,108 +160,37 @@ $(document).ready(function() {
 
 //        create_chart();
 //        new_chart();
-//        bubble_chart();
+        bubble_chart();
     }
 
 
     function bubble_chart() {
 
-        var text = "<div><canvas id='canvas4'></canvas></div>";
-        $("body").append(text);
-
-        var myChart = $("#canvas4");
-        var chart4 = new Chart(myChart, {
-            type: 'bubble',
-            data: {
-                datasets: [
-                {
-                    label: ["Fastball"],
-                    backgroundColor: "rgba(255,221,50,0.2)",
-                    borderColor: "rgba(255,221,50,1)",
-                    data: [{
-                        x: 9,
-                        y: 74,
-                        r: 57
-                    }]
-                },
-                {
-                    label: ["Change-Up"],
-                    backgroundColor: "rgba(60,186,159,0.2)",
-                    borderColor: "rgba(60,186,159,1)",
-                    data: [{
-                        x: 1,
-                        y: 71,
-                        r: 76
-                    }]
-                },
-                {
-                    label: ["Slider"],
-                    backgroundColor: "rgba(0,0,0,0.2)",
-                    borderColor: "#000",
-                    data: [{
-                        x: 13,
-                        y: 68,
-                        r: 42
-                    }]
-                },
-                {
-                    label: ["Curveball"],
-                    backgroundColor: "rgba(193,46,12,0.2)",
-                    borderColor: "rgba(193,46,12,1)",
-                    data: [{
-                        x: 4,
-                        y: 69,
-                        r: 48
-                    }]
-                }]
-            },
-            options: {
-                maintainAspectRatio: true,
-                responsive: false,
-                title: {
-                    display: true,
-                    text: 'Predicted strike percent'
-                },
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Average Velocity"
-                        }
-                    }],
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Pitch Count"
-                        }
-                    }]
-                }
-            }
-        });
-
-        var colors = [{"background":"rgba(255,221,50,0.2)","border":"rgba(255,221,50,1)"},
-                      {"background":"rgba(60,186,159,0.2)","border":"rgba(60,186,159,1)"},
-                      {"background":"rgba(193,46,12,0.2)","border":"rgba(193,46,12,1)"},
-                      {"background":"rgba(173,216,230,0.2)","border":"rgba(173,216,230,1)"}];
+        var colors = [{"background":"rgba(255,221,50,0.2)","border":"rgba(255,221,50,1)"}, // yellow
+                      {"background":"rgba(60,186,159,0.2)","border":"rgba(60,186,159,1)"}, // green
+                      {"background":"rgba(193,46,12,0.2)","border":"rgba(193,46,12,1)"}, // red
+                      {"background":"rgba(173,216,230,0.2)","border":"rgba(173,216,230,1)"}, // blue
+                      {"background":"rgba(104,19,214,0.2)","border":"rgba(104,19,214,1)"}, // purple
+                      {"background":"rgba(249,122,232,0.2)","border":"rgba(249,122,232,1)"}  // pink
+                      ];
         var datasets = [];
         var pitch_type = Object.keys(types);
 
         for (i = 0; i < pitch_type.length; i++) {
-
             var data_dict = {};
             data_dict["label"] = [pitch_type[i]];
             data_dict["data"] = [];
-            data_dict["backgroundColor"] = colors[i].background;
             data_dict["borderColor"] = colors[i].border;
+            data_dict["backgroundColor"] = colors[i].background;
+
             for (k = 0; k < pitching_data[pitch_type[i]].length; k++) {
                 var position = {};
                 position['x'] = pitching_data[pitch_type[i]][k].pitch_count;
+                position['r'] = 10;
                 if (pitching_data[pitch_type[i]][k].vel == 0) {
                     position['y'] = types[pitch_type[i]]["avg_velocity"];
-                    position['r'] = 5;
                 } else {
                     position['y'] = pitching_data[pitch_type[i]][k].vel;
-                    position['r'] = 10;
                 }
 
                 data_dict['data'].push(position);
@@ -265,11 +198,11 @@ $(document).ready(function() {
             datasets.push(data_dict);
         }
 
-        var text = "<div><canvas id='canvas5'></canvas></div>";
-        $("body").append(text);
+        var text = "<div><canvas id='canvas4'></canvas></div>";
+        $(".chart_container").append(text);
 
-        var myChart = $("#canvas5");
-        var chart5 = new Chart(myChart, {
+        var myChart = $("#canvas4");
+        var chart4 = new Chart(myChart, {
             type: 'bubble',
             data: {
                 datasets: datasets
@@ -300,131 +233,6 @@ $(document).ready(function() {
 
     }
 
-    function create_chart() {
-
-        var text = "<div><canvas id='canvas1'></canvas></div>";
-        $("body").append(text);
-
-        var pitch_types_array = Object.keys(types);
-        var counts = [];
-
-        for (i = 0; i < pitch_types_array.length; i++) {
-            counts.push(types[pitch_types_array[i]]['count']);
-        };
-
-        var myChart = $("#canvas1");
-        var chart1 = new Chart(myChart, {
-            type: 'pie',
-            data: {
-                labels: pitch_types_array,
-                datasets:[{
-                    label: "# of pitches",
-                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                    data: counts
-                }]
-            },
-            options: {
-                maintainAspectRatio: true,
-                responsive: false,
-                legend: { display: true },
-                title: {
-                    display: true,
-                    text: 'Pitch Count'
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
-        var text = "<div><canvas id='canvas2'></canvas></div>";
-        $("body").append(text);
-
-        var avg_velocities = [];
-
-        for (i = 0; i < pitch_types_array.length; i++) {
-            var vel_array = types[pitch_types_array[i]]['avg_velocity'];
-            var vel_sum = 0;
-
-            for (k = 0; k < vel_array.length; k++) {
-                vel_sum += Number(vel_array[k]);
-            }
-
-            var vel_avg = vel_sum/vel_array.length;
-            avg_velocities.push(vel_avg);
-        }
-
-        var myChart = $("#canvas2");
-        var chart2 = new Chart(myChart, {
-            type: 'bar',
-            data: {
-                labels: pitch_types_array,
-                datasets:[{
-                    label: "avg. velocity",
-                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                    data: avg_velocities
-                }]
-            },
-            options: {
-                maintainAspectRatio: true,
-                responsive: false,
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: 'Average Velocities'
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
-        var text = "<div><canvas id='canvas3'></canvas></div>";
-        $("body").append(text);
-
-        var strikes = [];
-
-        for (i = 0; i < pitch_types_array.length; i++) {
-            strikes.push(types[pitch_types_array[i]]['strike%']);
-        };
-
-        var myChart = $("#canvas3");
-        var chart3 = new Chart(myChart, {
-            type: 'bar',
-            data: {
-                labels: pitch_types_array,
-                datasets:[{
-                    label: "strike %",
-                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                    data: strikes
-                }]
-            },
-            options: {
-                maintainAspectRatio: true,
-                responsive: false,
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: 'Strike Percentage'
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    };
 
     function new_chart() {
 
